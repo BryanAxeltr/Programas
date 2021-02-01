e2d18acb11e7b603cdd062f1df3d9427dbcf5157
Perejiloo <bigottitos098@gmail.com>
2021-02-01 13:45:00 -0600

    a

---
+++ Recursividad/src/Recursividad/Array.java
@@ -0,0 +1,11 @@
+package Recursividad;
+//Recorrer un array de forma recursiva
+public class Array {
+private int arreglo[]={1,2,3,4,5,6};
+
+public void mostrarArray(int [] n,int indice){
+
+}
+
+
+}
